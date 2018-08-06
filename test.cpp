d09bc480e28de153c0c302174c430d4fff3206b2
ypkim <ypkim@ypkimui-MacBook-Pro.local>
2018-08-06 21:17:57 +0900

    first commit

---
+++ test.cpp
@@ -0,0 +1,16 @@
+\//
+//  test.cpp
+//  test
+//
+//  Created by ypkim on 2018. 8. 6..
+//
+
+#include "test.hpp"
+#include <iostream>
+int main()
+{
+    int a;
+    a = 10;
+    cout<<"Testing...%d",a;
+    return 0;
+}
